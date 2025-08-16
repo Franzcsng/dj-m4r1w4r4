2bf48cdfa48596e2fd9d1bf202d7daeb0778c542
Francis Causing <franzcsng23@gmail.com>
2025-08-16 23:31:38 +0800

    Added events card display in Events page with complete responsiveness

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,8 +1,27 @@
 import '../css/pages-style/Events.scss'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+//import { NavLink } from 'react-router-dom'
+import EventCard from '../components/home/event-section.js'; 
 import EventSlider from '../components/events/Event-Slider.js'
 import EventBanner from '../components/events/Event-Banner.js'
 
 const Evemts = () => {
+
+    const formatDate = (dateString) => {
+    return new Date(dateString).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        });
+    };
+    const [eventList, setEventList] = useState([]);
+    
+        useEffect(() =>{
+            axios.get("http://localhost:3001/events").then((response) =>{
+                setEventList(response.data)
+            })
+        }, [])
     return(
         <div className='events-main'>
             <EventBanner/>
@@ -20,7 +39,23 @@ const Evemts = () => {
                 </div>
 
                 <div className='events-list'>
+                    {eventList.map((value, key) =>{
+                        return(
+                        <>
+                            <EventCard 
+                        title= {value.eventName}
+                        address={value.eventAddress}
+                        desc="The Aurora Night Festival lit up the heart of the city with a dazzling fusion of lights, music, and community spirit. As the sun dipped below the skyline, strings of colorful lanterns illuminated the streets, guiding visitors toward the central plaza. Local artisans showcased their creations infhdrhdnb"
+                        date={formatDate(value.eventDate)}
+                        time="9PM - 5AM"
+                        />
+                        </>
+                        
 
+                        
+                       
+                        )
+                    })}
                 </div>
                
             </div>
