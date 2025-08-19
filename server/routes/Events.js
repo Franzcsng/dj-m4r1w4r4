bd3d21e05db2c2e410d458d848e1c1f21c3458a5
Francis Causing <franzcsng23@gmail.com>
2025-08-20 00:26:45 +0800

    Added completed put and delete event routes

--- server/routes/Events.js
+++ server/routes/Events.js
@@ -8,17 +8,64 @@ router.get('/', async (req, res) =>{
     res.json(allEvents)
 })
 
+router.get('/byId/:id', async (req, res) =>{
+    const id = req.params.id
+    const event = await Events.findByPk(id)
+    res.json(event)
+})
+
 router.post('/', async (req, res) =>{
     const event = req.body
     await Events.create(event)
     res.json(event)
 })
 
-
-router.get('/byId/:id', async (req, res) =>{
+router.put('/byId/:id', async (req, res) =>{
     const id = req.params.id
+    const {eventName, eventAddress, eventDate, eventTime, eventDescription, featuredEvent} = req.body
     const event = await Events.findByPk(id)
-    res.json(event)
+
+    if(event){
+        event.eventName =  eventName
+        event.eventAddress = eventAddress
+        event.eventDate = eventDate
+        event.eventTime = eventTime
+        event.eventDescription = eventDescription
+        event.featuredEvent = featuredEvent
+
+         if (featuredEvent === true) {
+            await Events.update(
+                { featuredEvent: false },
+                {
+                    where: {
+                        eventId: { [Op.ne]: id },   // not equal to current event
+                        featuredEvent: true
+                    }
+                }
+            )
+        }
+
+        await event.save()
+        res.json(event)
+
+    } else {
+        console.error('Error updating event' , err)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+router.delete('/byId/:id', async (req, res)=>{
+    try{
+        const id = req.params.id
+        await Events.destroy({
+        where: {eventId: id}
+        })
+        res.status(200).json({ message: "Event deleted succesfully"})
+    }catch(e){
+        console.error('Error deleting event:', err)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+    
 })
 
 
